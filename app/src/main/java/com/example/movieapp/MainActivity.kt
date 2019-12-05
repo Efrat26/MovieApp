e4b9f2d5f64ca3016d199969f1a378c4d7d4e0ad
efiso <efisofer26@gmail.com>
2019-12-05 16:45:30 +0200

    added intent for the button click

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.movieapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun linkToYoutube(view : View){
+
+        val webpage: Uri = Uri.parse("https://www.youtube.com/watch?v=6ZfuNTqbHE8")
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
