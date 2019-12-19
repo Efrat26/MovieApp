1e36f2a77e0952d06af53f000aaf920d351d5879
efiso <efisofer26@gmail.com>
2019-12-19 19:03:48 +0200

    added different sizes of the pictures, changed some attributes of the elements and changes the on click function to be more generic

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -5,20 +5,25 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var avengers_url = "https://www.youtube.com/watch?v=6ZfuNTqbHE8"
+    private val avengers_url = "https://www.youtube.com/watch?v=6ZfuNTqbHE8"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun linkToYoutube(view : View){
-
-        val webpage: Uri = Uri.parse(avengers_url)
-        val intent = Intent(Intent.ACTION_VIEW, webpage)
-        if (intent.resolveActivity(packageManager) != null) {
-            startActivity(intent)
+        var webpage : Uri = Uri.EMPTY
+        if(view.id == avengers_trailer_button.id) {
+            webpage = Uri.parse(avengers_url)
+        }
+        if(webpage != Uri.EMPTY) {
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
         }
     }
 }
