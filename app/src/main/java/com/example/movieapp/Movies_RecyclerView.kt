ce0912fbb6dec4700fe6f193559ff0b355b3231d
efiso <efisofer26@gmail.com>
2019-12-20 18:53:58 +0200

    added a recycler view + movie item layout + dimensions values file

---
+++ app/src/main/java/com/example/movieapp/Movies_RecyclerView.kt
@@ -0,0 +1,12 @@
+package com.example.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Movies_RecyclerView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies__recycler_view)
+    }
+}
